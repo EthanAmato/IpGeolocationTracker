f864a457443b60b1f3f2f4642f73cf2a546870f6
EthanAmato <ethannamato@gmail.com>
2023-02-10 14:49:11 -0500

    completion of project - only needs more updates for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { Marker } from 'react-leaflet'
 import { useMap } from 'react-leaflet'
-import { useMapEvent } from 'react-leaflet'
+import Arrow from './assets/images/icon-arrow.svg'
 import { Popup } from 'react-leaflet'
 import $ from 'jquery'
 
@@ -19,7 +19,7 @@ function App() {
     let userIpInput = ipRef.current.value;
 
     //Check user input using regex for 255.255.255.255 notation for IP addresses
-    if (/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(userIpInput)) {
+    if (/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(userIpInput.trim())) {
       $(function () {
         $.ajax({
           url: "https://geo.ipify.org/api/v2/country,city",
@@ -30,7 +30,7 @@ function App() {
           }
         }).then((res) => {
           console.log("In meme");
-          console.log(res.location)
+          console.log(res)
           setPos(() => [res.location.lat, res.location.lng])
           return [res.location.lat, res.location.lng]
         })
@@ -50,23 +50,42 @@ function App() {
     <>
       <main className='wrapper'>
         <header>
-          <h1>IP Address Tracker</h1>
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Search for any IP address or domain' ref={ipRef} />
-            <input type={'submit'} />
-          </form>
+          <div className='header-wrapper'>
+            <h1>IP Address Tracker</h1>
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder='Search for any IP address or domain' ref={ipRef} />
+              <input type={'submit'} value={" "} alt='Submit' />
+            </form>
+          </div>
         </header>
-        {/* <div className='results'>
-
-        </div> */}
+        {ipData && <div className='results'>
+          <table>
+            <thead>
+              <tr>
+                <th>IP ADDRESS</th>
+                <th>LOCATION</th>
+                <th>TIMEZONE</th>
+                <th>ISP</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{ipData.ip}</td>
+                <td>{ipData.location.city + ", " + ipData.location.region+" "+(ipData.location.postalCode || ipData.location.country)}</td>
+                <td>{"UTC " + ipData.location.timezone}</td>
+                <td>{ipData.isp}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>}
         <MapContainer center={pos} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        {ipData &&  <Marker position={pos}>
+          {ipData && <Marker position={pos}>
             <Popup className='popup'>
-              <h2 className='popup'>Welcome to {ipData.location.city + ", " + ipData.location.region}</h2><br />
+              <h2 className='popup'>Welcome to {ipData.location.city + ", " + ipData.location.country}</h2><br />
               <p className='popup'>Latitude: {pos[0]}, Longitude: {pos[1]}</p>
             </Popup>
           </Marker>}
